3aaf53a399853acf22e3c533272337fb492cbc04
Zenxdkd <99894254+Zenxdkd@users.noreply.github.com>
2023-06-07 21:33:30 +0200

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'cc paster','bruce wayne','drug lord','fraudstar','pinkcore ^_^','dont skid its bad!!'],
+  typeSpeed : 40,
+  delaySpeed : 1,
+  showCursor: false,
+  loop : true
+})
